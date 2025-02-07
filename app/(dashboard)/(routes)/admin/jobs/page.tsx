cc7e106c113db44ea5234b593a61aedfa3c1c627
Ansh Varma <anshvarma83691@gmail.com>
2025-02-07 23:50:00 +0530

    jobs

--- app/(dashboard)/(routes)/admin/jobs/page.tsx
+++ app/(dashboard)/(routes)/admin/jobs/page.tsx
@@ -1,8 +1,22 @@
+import { Button } from '@/components/ui/button'
+import { Plus } from 'lucide-react'
+import Link from 'next/link'
 import React from 'react'
 
-const page = () => {
+const page = async () => {
   return (
-    <div>page</div>
+    <div className='p-6 '>
+      <div className='flex items-end justify-end'>
+        <Link href={"/admin/create"}>
+          <Button>
+            <Plus className='h-5 w-5 mr-2'/>
+             New Job
+          </Button>
+        </Link>
+      </div>
+
+      {/* DataTable - List of Jobs */}
+    </div>
   )
 }
 
