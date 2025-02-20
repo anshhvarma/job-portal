8a5b49a7d48e60b2a26c6b66a29cc74f023807a4
Ansh Varma <anshvarma83691@gmail.com>
2025-02-20 22:01:04 +0530

    company

---
+++ app/(dashboard)/(routes)/admin/companies/_components/columns.tsx
@@ -0,0 +1,81 @@
+"use client";   
+import { ColumnDef } from "@tanstack/react-table";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+  } from "@/components/ui/dropdown-menu"
+import { Button } from "@/components/ui/button";
+import { ArrowUpDown, Eye, MoreHorizontal, Pencil } from "lucide-react";
+import Link from "next/link";
+  
+
+
+export type CompanyColumns = {
+  id: string;
+  name: string;
+  logo: string;
+  createdAt: string;
+};
+
+export const columns: ColumnDef<CompanyColumns>[] = [
+  {
+    accessorKey: "logo",
+    header: "Logo"
+  },
+  {
+    accessorKey: "name",
+    header: ({ column }) => {
+        return (
+          <Button
+            variant="ghost"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Name
+            <ArrowUpDown className="ml-2 h-4 w-4" />
+          </Button>
+        )
+      },
+  },
+ 
+  {
+    accessorKey: "createdAt",
+    header: ({ column }) => {
+        return (
+          <Button
+            variant="ghost"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Date
+            <ArrowUpDown className="ml-2 h-4 w-4" />
+          </Button>
+        )
+      },
+  },
+  {
+    accessorKey: "Options",
+    id : "actions",
+    cell:({row}) => {
+        const {id} = row.original;
+        return(
+    <DropdownMenu>
+  <DropdownMenuTrigger asChild>
+    <Button variant={"ghost"} size={"icon"}>
+        <MoreHorizontal className="h-4 w-4"/>
+    </Button>
+  </DropdownMenuTrigger>
+  <DropdownMenuContent>
+    <Link href={`/admin/companies/${id}`}>
+            <DropdownMenuItem>
+                <Pencil className="h-4 w-4 mr-2" />
+                Edit
+            </DropdownMenuItem>
+    </Link>
+  </DropdownMenuContent>
+</DropdownMenu>
+
+        )
+    },
+  },
+];
